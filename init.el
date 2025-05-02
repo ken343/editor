3f7a70902dbf297521b86e529682d20890b1f563
Ken <kmcdole21@gmail.com>
2025-05-01 21:06:46 -0500

    Python IDE Environment

--- init.el
+++ init.el
@@ -1,46 +1,99 @@
-;; INIT FILE
+;;; package --- Summary -*- lexical-binding: t -*-
 
-;; (scroll-bar-mode -1) ; Remove scroll bar
-(tool-bar-mode -1)      ; Disable the toolbar
-(tooltip-mode -1)       ; Disable the tooltips
-(set-fringe-mode 10)    ; Give some breathing room
-(menu-bar-mode -1)      ; Disable the menu bar
+;;; Commentary:
+;;; I don't know what to type.
 
-(setq visual-bell t)
+;;; Code:
+(setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Ubuntu Mono" :height 180)
+(when (>= emacs-major-version 28)
+  (setq lock-file-name-transforms
+        '(("\\`/.*/\\([^/]+\\)\\'" "~/.emacs.d/aux/\\1" t))))
+(setq auto-save-file-name-transforms
+      '(("\\`/.*/\\([^/]+\\)\\'" "~/.emacs.d/aux/\\1" t)))
+(setq backup-directory-alist
+      '((".*" . "~/.emacs.d/aux/")))
 
-(load-theme 'tango-dark)
-
-;; Escape will quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Initialize package sources
+;; Ensure package initialization if not already done
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+(setq package-enable-at-startup nil) ; To avoid initializing twice
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Make sure to reload a package unless it exists already
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Make sure use-package is installed on non-linux systems
+;; Ensure use-package is installed
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
-(setq use-package-alwayse-ensure t)
 
+;; LSP setup with lsp-mode and lsp-pyright (Pyright installed globally via npm)
+(use-package lsp-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred)  ; lsp-deferred launches LSP when needed
+  :commands (lsp lsp-deferred)
+  :config
+  (setq lsp-keymap-prefix "C-c l")    ; Optional: set a custom LSP prefix key
+  (setq lsp-enable-snippet t))
+
+(use-package lsp-pyright
+  :ensure t
+  :after lsp-mode
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp-deferred)))   ; Use lsp-deferred for a more responsive startup
+  ;; Optional: customize lsp-pyright here
+  :config
+  (setq lsp-pyright-multi-root nil))
+
+;; Company mode for auto-completion
+(use-package company
+  :ensure t
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.0))  ; immediately show suggestions
+
+;; Flycheck for on-the-fly syntax checking
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; Blacken for automatic code formatting with Black
+(use-package blacken
+  :ensure t
+  :hook (python-mode . blacken-mode)
+  :config
+  (setq blacken-line-length 88))
+
+;; pyvenv for managing Python virtual environments
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode 1)
+  ;; Optionally set the default virtual environment
+  ;; (pyvenv-activate "/path/to/your/venv")
+  )
+
+;; Optional: automatically activate a venv based on directory
+(use-package direnv
+  :ensure t
+  :config
+  (direnv-mode))
+
+;; CUSTOMIZE
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(custom-enabled-themes '(modus-vivendi-tritanopia))
+ '(package-selected-packages '(blacken direnv elpy flycheck ht lsp-mode lsp-pyright lv)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+;;; init.el ends here
